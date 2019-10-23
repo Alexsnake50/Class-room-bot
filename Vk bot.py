c944fe4f8321843c7d3c0f6a043cc7c09712760d
Alexnsake50 <51927622+Alexnsake50@users.noreply.github.com>
2019-10-23 22:29:54 +0300

    Бета бранч недельного апдейта
    Недельный апдейт с новыми функциями

--- Vk bot.py
+++ Vk bot.py
@@ -3,6 +3,7 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from datetime import datetime
 import random
 import time
+from Meta_Data import Name, mt, Metadata 
 vk_session = vk_api.VkApi(token='83b4c5cd93ff81cab83c0b4d85985aa648df73bf96d43f0eb1ec602a6bd738b8df5484893b658db0aebd0')
 session_api = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, 183006227) 
@@ -18,39 +19,35 @@ while True:
             print('Текст:', event.obj.text)
             print()
             if event.from_user:
-                if event.obj.text == "Неделя 1":
-                    week = 1
-                elif event.obj.text == "Неделя 2":
-                    week = 2
-                if event.obj.text == "Понедельник" or event.obj.text == "понедельник":
+                for x in Name:
+                    if x == event.obj.text:
+                        DataName = Name.index(x)
+                        if DataName == 2 or DataName == 3 or DataName == 8 or DataName == 9 and week == 2:
+                            m = 0
+                            if DataName == 2 or DataName == 3 :
+                                m = mt.Tuesday2
+                            elif DataName == 8 or DataName == 9 :
+                                m = mt.Friday2
+                            session_api.messages.send(
+                                user_id=event.obj.from_id,
+                                random_id = 0,
+                                message=m
+                            )
+                        else :
+                            session_api.messages.send(
+                                user_id=event.obj.from_id,
+                                random_id = 0,
+                                message= Metadata[DataName]
+                            )
+                if Name.count(event.obj.text)  == 0 and event.obj.text != "Начать" :
                     session_api.messages.send(
                         user_id=event.obj.from_id,
                         random_id = 0,
-                        message= '----' '<br>' 'Безопасность жизнедеятельности: каб.212' '<br>' 'Иностранный язык: каб.207' '<br>' 'Теория вероятности и математическая статистика: каб.214' '<br>' '----'
-                )
-                if event.obj.text == "Вторник" or event.obj.text == "вторник":
-                    session_api.messages.send(
-                        user_id=event.obj.from_id,
-                        random_id = 0,
-                        message= ""
+                        message= "Такой команды к сожалению нету :*<",
                     )
-                if event.obj.text == "Среда" or event.obj.text == "среда":
+                if event.obj.text == "Начать": 
                     session_api.messages.send(
                         user_id=event.obj.from_id,
                         random_id = 0,
-                        message=""
-                    )
-                if event.obj.text == "Четверг" or event.obj.text == "четверг":
-                    session_api.messages.send(
-                        user_id=event.obj.from_id,
-                        random_id = 0,
-                        message=""
-                    )
-                if event.obj.text == "Пятница" or event.obj.text == "пятница":
-                    session_api.messages.send(
-                        user_id=event.obj.from_id,
-                        random_id = 0,
-                        message=""
-                    )
-                print(week);
-                
+                        message= "Команды для получения расписания, это всего лишь название дня недели на которое вы хотите получить расписание, к примеру “Понедельник” заглавность первой буквы не играет роли." "<br>" "Если вы хотите получить расписание на всю неделю, напишите “Неделя”, однако неделя может работать немного не так как нужно",
+                    )     
