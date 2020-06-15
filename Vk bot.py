e3a510d7c9b59c4a5714f50c3c0c3a1e7e569935
Alexnsake50 <51927622+Alexnsake50@users.noreply.github.com>
2020-06-15 16:51:14 +0300

    Fix

--- Vk bot.py
+++ Vk bot.py
@@ -3,12 +3,20 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from datetime import datetime, date
 import random
 import time
+<<<<<<< Updated upstream
 from Meta_Data import mt, Name, Metadata
 vk_session = vk_api.VkApi(token='83b4c5cd93ff81cab83c0b4d85985aa648df73bf96d43f0eb1ec602a6bd738b8df5484893b658db0aebd0')
 session_api = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, 183006227)
 longpoll = VkBotLongPoll(vk_session, 183006227) 
 week = 0 
+=======
+from Meta_Data import Name, mt, Metadata 
+vk_session = vk_api.VkApi(token=)
+session_api = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session,) 
+week = 0
+>>>>>>> Stashed changes
 dt = datetime.now()
 array = ["Как же не хочется на пары","Скоро сессия, а может и нет","Скоро отчисление","Прятного дня, но это не та команда","Опять бух учёт !","Нет, завтра не выходной", "Прошу прощения, вы ошиблись","Извините, но такой команды нету", "Пидрила ебанный", "уёбак сранный","Хуесас дебил", "Нету такой команды","СКАЗАЛИ ЖЕ НЕТУ ТАКОЙ КОМАНДЫ","Ты дебил ?","НЕТ ТАКОЙ КОМАНДЫ !", "Уди отсюда","Ты это, это, НЕ ИСПЫТЫВАЙ МЕНЯ"]
 if dt.isocalendar()[1] % 2 == 0:
@@ -30,7 +38,16 @@ while True:
                 for x in Name:
                     if x == event.obj.text:
                         DataName = Name.index(x)
+<<<<<<< Updated upstream
                         if DataName == 2 or DataName == 3 or DataName == 8 or DataName == 9 and week == 2:
+=======
+                        if (DataName == 2 or DataName == 3 or DataName == 8 or DataName == 9) and week == 2:
+                            m = 0
+                            if DataName == 2 or DataName == 3 :
+                                m = mt.Tuesday2
+                            elif DataName == 8 or DataName == 9 :
+                                m = mt.Friday2
+>>>>>>> Stashed changes
                             session_api.messages.send(
                                 user_id=event.obj.from_id,
                                 random_id = 0,
